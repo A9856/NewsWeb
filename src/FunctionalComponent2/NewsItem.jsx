a6f5460f583d0de860af08ab34331c5b835a37c1
A9856 <adilkhanofficial74@gmail.com>
2025-06-04 11:58:23 +0530

    NewsItem.jsx

--- src/FunctionalComponent2/NewsItem.jsx
+++ src/FunctionalComponent2/NewsItem.jsx
@@ -6,7 +6,7 @@ export default function NewsItem(props) {
     <div className='col-xl-2 col-lg-3 col-md-4 col-sm-6'>
     <div className="card">
         {/* <img src={props.pic?props.pic:"/images/noimage.png"}height={200} className="card-img-top" alt="..." /> */}
-        <img src={props.pic?props.pic:`${process.env.PUBLIC_URL}/NewsWeb/images/noimage.png`}height={200} className="card-img-top" alt="..." />
+        <img src={props.pic?props.pic:`${process.env.PUBLIC_URL}/images/noimage.png`}height={200} className="card-img-top" alt="..." />
         <div className="card-body">
             <h5 className="card-title">{props.title}</h5>
             <div className='source'>
